3e2ead684df72a087418ee336047fb3cc3ac3768
Gavin Garza <48096041+RenGVZ@users.noreply.github.com>
2020-08-24 23:35:52 +0900

    added turn switching functionality

--- src/board.jsx
+++ src/board.jsx
@@ -5,28 +5,38 @@ class Board extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
   handleClick = (i) => {
     const squares = this.state.squares.slice();
-    console.log(squares)
-    squares[i] = "X";
-    this.setState({squares: squares})
-
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
   }
   renderSquare(i) {
-    return ( 
+    return (
       <Square 
         value={this.state.squares[i]} 
         onClick={() => this.handleClick(i)}
       />
-    )
+    );
   }
 
   render() {
-    const status = 'Next player: X';
-
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = "Winner:" + winner;
+    } else {
+      status = `Next Player: ${this.state.xIsNext ? "X" : "O"}`;
+    }
     return (
       <div>
         <div className="status">{status}</div>
@@ -50,4 +60,25 @@ class Board extends Component {
   }
 }
 
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    let [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === 
+        squares[b] && squares[a] ===
+        squares[c]) {
+          return squares[a];
+        }
+  }
+  return null;
+}
 export default Board;
--- src/square.jsx
+++ src/square.jsx
@@ -1,18 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Square extends Component {
-  // handleClick = () => {
-  //   return this.props.value === "X"
-  // }
-  render() {
-    return (
-      <button 
-        className="square" 
-        onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+const Square = (props) => {
+  return (
+    <button 
+      className="square"
+      onClick={props.onClick} >
+      {props.value}
+    </button>
+  );
 }
 
 export default Square;
