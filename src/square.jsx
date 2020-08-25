24209849415171e897c45a0025d36680517f2a2a
Gavin Garza <48096041+RenGVZ@users.noreply.github.com>
2020-08-25 23:15:21 +0900

    reviewing tic-tac toe

--- src/square.jsx
+++ src/square.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Square = (props) => {
-  return (
-    <button 
-      className="square"
-      onClick={props.onClick} >
-      {props.value}
-    </button>
-  );
+class Square extends Component {
+  render() {
+    return (
+      <button 
+        className="square" 
+        onClick={() => this.props.onClick()}> 
+        {this.props.value}
+      </button>
+    );
+  }
 }
 
 export default Square;
